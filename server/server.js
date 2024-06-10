2c6d0d77cd50a8cc8ba982f1a010c89c184a0431
cwchang2258 <cw.chang@anfusolutions.com>
2024-06-10 22:57:34 +0800

    port 7860

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const WebSocket = require('ws');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7860;
 const wsPort = process.env.WS_PORT || 3001;
 
 
